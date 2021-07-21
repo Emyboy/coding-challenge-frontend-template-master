094ae1bfbffd51982ec777ce2cc9322f38e4a4c5
emykumba <emy@meetkumba.com>
2021-07-22 00:26:09 +0100

    Implemented the search feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,17 +5,30 @@ import UserCard from "../components/user.card";
 import { fetchStudent, fetchStudents, Student } from "../services/students";
 
 type Props = {
-  students: Student[];
+  studentsList: Student[];
 };
 
-const Main: React.FC<Props> = ({ students }) => {
-  const onChangeHandler = (event: any) => {
-    // TODO
+const Main: React.FC<Props> = ({ studentsList }) => {
+
+  const [searchKeyword, setSearchKeyword] = useState<string>('');
+  const [students, setStudent] = useState<Student[]>(studentsList);
+
+  const onChangeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchKeyword(event.target.value);
   };
+
+  useEffect(() => {
+    if (searchKeyword) {
+      setStudent(studentsList.filter(student => student.first_name.toLocaleLowerCase().includes(searchKeyword)
+        || student.last_name.toLocaleLowerCase().includes(searchKeyword)))
+    } else {
+      setStudent(studentsList)
+    }
+  }, [searchKeyword])
 
   return (
     <Box direction="column" pad="medium" height="100%" overflow="auto">
-      <TextInput placeholder="type here" value="" onChange={onChangeHandler} />
+      <TextInput placeholder="Search: First or Last Name" value={searchKeyword} onChange={onChangeHandler} />
       <Box direction="row" wrap={true}>
         {students.map((s) => (
           <Box margin="10px" key={s.id}>
@@ -29,11 +42,11 @@ const Main: React.FC<Props> = ({ students }) => {
 
 export const getServerSideProps: GetServerSideProps = async () => {
 
-  const students:Student[] = await fetchStudents();
+  const studentsList: Student[] = await fetchStudents();
 
   return {
     props: {
-      students,
+      studentsList,
     },
   };
 
